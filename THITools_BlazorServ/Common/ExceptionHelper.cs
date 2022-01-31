8c23064fc6d0d307e007fbbd05bd3e42c5e17800
Laura Ann Truax <lauraanntruax@gmail.com>
2022-01-31 14:32:36 -0500

    Custom Error work

--- THITools_BlazorServ/Common/ExceptionHelper.cs
+++ THITools_BlazorServ/Common/ExceptionHelper.cs
@@ -9,6 +9,7 @@ namespace THITools.Common
     {
         Exception ex {get; set;}
         string user { get; set; }
+        string environment { get; set; }
         string customMessage { get; set; }
         DateTime exceptionTime { get; set; }
         bool logged { get; set; }
@@ -17,6 +18,7 @@ namespace THITools.Common
         {
             Exception ex = new Exception();
             string user = ""; //TODO: Add user for error logging
+            string environment = ""; //TODO: Add environment info
             string customMessage = "";  //TODO: Add custom message for error logging
             DateTime exceptionTime = DateTime.Now;
             bool logged;
@@ -26,6 +28,7 @@ namespace THITools.Common
         {
             Exception ex = e;
             string user = ""; //TODO: Add user for error logging
+            string environment = ""; //TODO: Add environment info
             string customMessage = ""; //TODO: Add custom message for error logging
             DateTime exceptionTime = DateTime.Now;
             bool logged;
@@ -33,8 +36,17 @@ namespace THITools.Common
 
         public bool LogException()
         {
-            //TODO: implement LogException logic - add error log record
-            return false;
+            try
+            {
+                //TODO: implement LogException logic - add error log record
+                //
+                return true; //return true if error log is written to DB 
+            }
+            catch (Exception ex)
+            {
+                
+                return false;
+            }
         }
     }
 }
