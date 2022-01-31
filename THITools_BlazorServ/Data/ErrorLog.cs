6149b12fd07bce018bd5b9f957406faaef20e9fb
Laura Ann Truax <lauraanntruax@gmail.com>
2022-01-31 17:08:38 -0500

    Adding SQL Data Access with Dapper

--- THITools_BlazorServ/Data/ErrorLog.cs
+++ THITools_BlazorServ/Data/ErrorLog.cs
@@ -1,14 +1,36 @@
-﻿namespace THITools_BlazorServ.Data
+﻿using THITools_BlazorServ.DbAccess;
+using THITools_BlazorServ.Models;
+
+namespace THITools_BlazorServ.Data
 {
     public class ErrorLog
     {
-        public int Id { get; set; }
-        public int ModuleId { get; set; }     
-        public DateTime ErrorTimestamp { get; set; }    
+        
 
-        public string ErrorUser { get; set; }
-        public string ErrorMsg { get; set; }
-        public int ErrorSeverity { get; set; }
-        public int ErrorState { get; set; }
+        private readonly ISqlDataAccess _db;
+
+        public ErrorLog(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<IEnumerable<ErrorLogModel>> GetErrorLogs() =>
+            _db.LoadData<ErrorLogModel, dynamic>(storedProcedure: "dbo.spErrorLog_GetAll", new { });
+
+        public async Task<ErrorLogModel?> GetErrorLog(int id)
+        {
+            var results = await _db.LoadData<ErrorLogModel, dynamic>(storedProcedure: "dbo.spErrorLog_Get", new { Id = id });
+            return results.FirstOrDefault();
+        }
+
+        public Task InsertErrorLog(ErrorLogModel error) =>
+            _db.SaveData(storedProcedure: "dbo.spErrorLog_Insert", new { error.ModuleId, error.ErrorTimestamp, error.ErrorUser, error.ErrorMsg, error.ErrorSeverity, error.ErrorState });
+
+        public Task UpdateErrorLog(ErrorLogModel error) =>
+            _db.SaveData(storedProcedure: "dbo.spErrorLog_Update", error);
+
+        public Task DeleteErrorLog(int id) =>
+            _db.SaveData(storedProcedure: "dbo.spErrorLog_Delete", new { Id = id });
+
+
     }
 }
---
+++ THITools_BlazorServ/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace THITools_BlazorServ.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
