47ffff95b09713bd1cb81812189a769843dba56b
Laura Ann Truax <lauraanntruax@gmail.com>
2022-02-02 09:47:17 -0500

    More DB access and added System.DirectoryServices.AccountManagement package

---
+++ THITools_BlazorServ/Models/RoleCategoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace THITools_BlazorServ.Models
+{
+    public class RoleCategoryModel
+    {
+        public int Id { get; set; }
+        public string RoleCategory { get; set; }
+        public bool Inactive { get; set; }
+
+    }
+}
